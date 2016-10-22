94c33ea42314e26eecceca4f374b51f3103f7e29
PAULGAMING\Paul <paul.me.smith@gmail.com>
2016-10-22 14:47:40 -0500

    no message

---
+++ Assets/Scripts/EncounterAreaNewBehaviourScript.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EncounterArea : MonoBehaviour {
+
+    public GameObject prefabToSpawn;
+    public float minimumSpawnTime;
+    public void SetTrap(GameObject lure)
+    {
+        prefabToSpawn = lure.GetComponent<LurePotion>;
+        Destroy(lure);
+        //Invoke("Grow", minimumSpawnTime + Random.Range(minimumSpawnTime / 2, minimumSpawnTime * 1.5f));
+        StartCoroutine(delayedGrow(minimumSpawnTime + Random.Range(minimumSpawnTime / 2, minimumSpawnTime * 1.5f), prefabToSpawn));
+    }
+    IEnumerator delayedGrow(float delay, GameObject toSpawn)
+    {
+        yield return new WaitForSeconds(delay);
+        Grow();
+    }
+    void Grow()
+    {
+        int randomNumToSpawn = Random.Range(1, 5);
+        for (int i = 0; i < randomNumToSpawn; i++)
+        {
+            Vector3 random = new Vector3(Random.value * 2 - 1, 0.01f, Random.value * 2 - 1);
+            random *= growRadius;
+            Instantiate(prefabToSpawn, transform.position + random, Quaternion.identity);
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        // If not a seed, return
+        if (other.GetComponent<Seed>() == null)
+        {
+            return;
+        }
+        PlantSeed(other.gameObject);
+    }
+}
---
+++ Assets/Scripts/LurePotion.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LurePotion : MonoBehaviour {
+
+    public enum LureType { level1, level2 }
+    public LureType lureType;
+    public GameObject prefabToSpawn0;
+    public GameObject prefabToSpawn1;
+    public GameObject prefabToSpawn2;
+    public GameObject prefabToSpawn3;
+    public GameObject getPrefabToSpawn()
+    {
+        int random = Mathf.RoundToInt(Random.Range(0f, 3f));
+        switch (random){
+            case 0:
+                return prefabToSpawn0;
+            case 1:
+                return prefabToSpawn1;
+            case 2:
+                return prefabToSpawn2;
+            default:
+                return prefabToSpawn3;
+            
+        }
+    }
+}
