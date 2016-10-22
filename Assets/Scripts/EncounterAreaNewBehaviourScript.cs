386528e8b534334130cc94b382d0888367de90a5
PAULGAMING\Paul <paul.me.smith@gmail.com>
2016-10-22 14:48:16 -0500

    no message

--- Assets/Scripts/EncounterAreaNewBehaviourScript.cs
+++ Assets/Scripts/EncounterAreaNewBehaviourScript.cs
@@ -7,34 +7,29 @@ public class EncounterArea : MonoBehaviour {
     public float minimumSpawnTime;
     public void SetTrap(GameObject lure)
     {
-        prefabToSpawn = lure.GetComponent<LurePotion>;
+        prefabToSpawn = lure.GetComponent<LurePotion>().getPrefabToSpawn();
         Destroy(lure);
         //Invoke("Grow", minimumSpawnTime + Random.Range(minimumSpawnTime / 2, minimumSpawnTime * 1.5f));
-        StartCoroutine(delayedGrow(minimumSpawnTime + Random.Range(minimumSpawnTime / 2, minimumSpawnTime * 1.5f), prefabToSpawn));
+        StartCoroutine(delayedGrow(minimumSpawnTime, prefabToSpawn));
     }
     IEnumerator delayedGrow(float delay, GameObject toSpawn)
     {
         yield return new WaitForSeconds(delay);
-        Grow();
+        spawn();
     }
-    void Grow()
+    void spawn()
     {
         int randomNumToSpawn = Random.Range(1, 5);
-        for (int i = 0; i < randomNumToSpawn; i++)
-        {
-            Vector3 random = new Vector3(Random.value * 2 - 1, 0.01f, Random.value * 2 - 1);
-            random *= growRadius;
-            Instantiate(prefabToSpawn, transform.position + random, Quaternion.identity);
-        }
+        Instantiate(prefabToSpawn, transform.position, Quaternion.identity);
     }
 
     void OnTriggerEnter(Collider other)
     {
         // If not a seed, return
-        if (other.GetComponent<Seed>() == null)
+        if (other.GetComponent<LurePotion>() == null)
         {
             return;
         }
-        PlantSeed(other.gameObject);
+        SetTrap(other.gameObject);
     }
 }
