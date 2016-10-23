77ba532d056dbff7c812faa5c4701ae87633df48
Toni Carlone <Toni Carlone>
2016-10-23 15:40:44 -0500

    Do book recipies

--- Assets/OutOfBounds.cs
+++ Assets/OutOfBounds.cs
@@ -3,8 +3,14 @@ using System.Collections;
 
 public class OutOfBounds : MonoBehaviour {
 	public RespawnManager respawnManager;
+	public Transform cauldronCenter;
+	public float cauldronRadius;
 	void OnTriggerEnter(Collider other)
 	{
+		if (other.tag.Equals("Paddle")) {
+			if (Vector3.Distance (other.transform.position, cauldronCenter.position) < cauldronRadius)
+				return;
+		}
 		Rigidbody otherRigidbody = other.GetComponentInParent<Rigidbody>();
 		if (otherRigidbody && otherRigidbody.gameObject)
 			respawnManager.Respawn (otherRigidbody.gameObject);
