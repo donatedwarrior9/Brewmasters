68d05a6c26df643b1c5a3a4ed6a9e21895afe40b
PAULGAMING\Paul <paul.me.smith@gmail.com>
2016-10-23 13:25:56 -0500

    seed stuff

--- Assets/Scripts/Seed.cs
+++ Assets/Scripts/Seed.cs
@@ -3,7 +3,7 @@ using System.Collections;
 
 public class Seed : MonoBehaviour {
 
-    public enum SeedType {FingerRoot, MoonWort, RedMushroom, BrownMushroom, Nyteshaid}
+    public enum SeedType {FingerRoot, MoonWort, Nightshade, Pumpkin}
     public SeedType seedType;
     public GameObject prefabToSpawn;
     public GameObject getPrefabToSpawn()
